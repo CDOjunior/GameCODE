0256705e13097116a17ab2c1a3f2b201a1c3c51a
Celso Junior <juniorcelso791@gmail.com>
2025-03-08 12:38:07 -0400

    As Entidades Usuario e Insignias foram criadas

---
+++ Models/Entities/Insignia.cs
@@ -0,0 +1,19 @@
+﻿namespace GameCODE.Models.Entities
+{
+    public class Insignia
+    {
+        public Insignia(string nome, string descricao, string imagem)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Imagem = imagem;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public Usuario Usuario { get; set; } // propriedade para conseguir acessar o usuário através de uma insignia
+        public int UsuarioId { get; set; } // propriedade para mapear quem tem uma determinada insignia
+    }
+}
---
+++ Models/Entities/Usuario.cs
@@ -0,0 +1,24 @@
+﻿namespace GameCODE.Models.Entities
+{
+    public class Usuario
+    {
+        public Usuario(string nome, string email, string senha, string tipo, string atuacao)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Tipo = tipo;
+            Atuacao = atuacao;
+            Insignias = new List<Insignia>(); // ter a possibilidade de ter insignias
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Tipo { get; set; }
+        public string Atuacao { get; set; }
+        public List<Insignia> Insignias { get; set; }
+
+    }
+}
